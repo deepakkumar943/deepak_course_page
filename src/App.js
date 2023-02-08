8b82fc7989f154a52f20c914cf7709cf740a9c0e
deepak <deepakkumarram943@gmail.com>
2023-02-08 21:23:27 +0530

    my first accadmic_course

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// eslint-disable-line
+//import Physics from './images/physics.jpg';
+import './App.css';
+
+const App = ()=>{
+  return (
+    <div className="App">
+      <div>
+        <h1 className="bg">IIT JEE</h1>
+        <h1>Upcoming Course</h1>
+      </div>
+    </div>
+  );
+}
+export default App;
